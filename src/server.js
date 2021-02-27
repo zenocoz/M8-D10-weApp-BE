1c62c277eab3c5ebc384b2db47f85a7b35a33e63
zenocoz <federico.soncini@gmail.com>
2021-02-27 19:05:42 +0100

    changed cors policy

--- src/server.js
+++ src/server.js
@@ -27,6 +27,16 @@ server.use(
   })
 )
 
+// Add Access Control Allow Origin headers
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
 // server.use(
 //   cors({
 //     origin: function (origin, callback) {
